2d3477ef743876de25a2a480dd08b0cc99ac52f5
pashu03 <74955102+pashu03@users.noreply.github.com>
2025-04-20 22:33:25 +0530

    Update thread.tsx
    + Added

--- components/assistant-ui/thread.tsx
+++ components/assistant-ui/thread.tsx
@@ -169,9 +169,11 @@ const ComposerAction: FC = () => (
 
 const UserMessage: FC = () => (
   <MessagePrimitive.Root className="grid auto-rows-auto grid-cols-[minmax(72px,1fr)_auto] gap-y-2 [&:where(>*)]:col-start-2 w-full max-w-[var(--thread-max-width)] py-4">
-  
+    
   </MessagePrimitive.Root>
 );
+
+
 
 
 const CircleStopIcon = () => (
